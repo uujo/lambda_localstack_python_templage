bd57926c803589a891616a8c2c3ac02a8767d517
seangchan-ryu-nih <seangchan.ryu@nih.gov>
2020-02-26 15:38:23 -0500

    add unittests
    add test

---
+++ tests/unittests/awshelper_t/test_awsenv.py
@@ -0,0 +1,45 @@
+"""
+Test awsenv module. mocking aws ssm access
+"""
+import json
+from unittest.mock import patch
+from botocore.exceptions import ClientError
+from projectname.awshelper.awsenv import get_params_from_ssm, get_params_from_secretsmanager
+
+
+@patch("projectname.awshelper.awsenv.boto3.Session")
+def test_get_ssm_success(boto3_session):
+    """
+    Test successful case when get_params_from_ssm returns param
+    :param boto_session:
+    :return: None
+    """
+    PARAM = {'Parameter': {'Value': json.dumps({"TEST_TABLE_NAME": "TEST_TABLE"})}}
+    boto3_session().client().get_parameter.return_value = PARAM
+
+    response = get_params_from_ssm("/test/config")
+    assert response == json.loads(PARAM['Parameter']['Value'])
+
+    # When param doesn't exists
+    boto3_session().client().get_parameter.return_value = {}
+    response = get_params_from_ssm("/test/config")
+    assert response == {}
+
+
+@patch("projectname.awshelper.awsenv.boto3.Session")
+def test_get_param_success(boto3_session):
+    """
+    Test successful case when get_params_from_ssm returns param
+    :param boto_session:
+    :return: None
+    """
+    PARAM = {'SecretString': json.dumps({'SOME_SECRET': "abc"})}
+    boto3_session().client().get_secret_value.return_value = PARAM
+
+    response = get_params_from_secretsmanager("/test/config")
+    # assert response == json.loads(PARAM['SecretString'])
+
+    # When param doesn't exists
+    boto3_session().client().get_secret_value.return_value = {}
+    response = get_params_from_secretsmanager("/test/config")
+    assert response == {}
